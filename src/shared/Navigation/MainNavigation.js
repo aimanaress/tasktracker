ba94ef4a850589f3f27e557f92cd001c65583de6
Aiman Ares <aaimanaress@gmail.com>
2024-02-24 23:12:52 +0800

    Update botton navigation

--- src/shared/Navigation/MainNavigation.js
+++ src/shared/Navigation/MainNavigation.js
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import NavLinks from "./NavLinks";
+import {
+  AdjustmentsHorizontalIcon,
+  CalendarDaysIcon,
+  GlobeAltIcon,
+} from "@heroicons/react/24/outline";
 import "./MainNavigation.css";
 
 function MainNavigation() {
+  const [active, setActive] = useState("Today");
+
+  const activeHandler = (title) => {
+    // Update active state with the title of the clicked navigation
+    setActive(title);
+  };
+
+  const navigations = [
+    {
+      title: "Today",
+      icon: AdjustmentsHorizontalIcon,
+    },
+    {
+      title: "Weekly",
+      icon: CalendarDaysIcon,
+    },
+    {
+      title: "Overall",
+      icon: GlobeAltIcon,
+    },
+  ];
+
   return (
     <div className="container__mainnav">
-      <NavLinks />
-      <NavLinks />
-      <NavLinks />
+      {navigations.map((nav) => {
+        return (
+          <NavLinks
+            title={nav.title}
+            icon={<nav.icon />}
+            // Check if the navigation is active
+            show={nav.title === active}
+            // Pass the title to activeHandler
+            onClick={() => activeHandler(nav.title)}
+          />
+        );
+      })}
     </div>
   );
 }
--- src/shared/Navigation/NavLinks.js
+++ src/shared/Navigation/NavLinks.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./NavLinks.css";
 
-function NavLinks() {
+function NavLinks(props) {
   return (
-    <div className="container__navlink">
-      <p>ICON</p>
-      <p>Icon Name</p>
-    </div>
+    <a
+      className={`container__navlink ${props.show && "current"} `}
+      onClick={props.onClick}
+    >
+      {props.icon}
+      <p>{props.title}</p>
+    </a>
   );
 }
 
